75517159a1f056e47afabae07458168218d52522
Peter Moran <workingpeter@gmail.com>
2010-03-16 10:24:59 +1100

    Minor member script refactor

--- examples/members.rb
+++ examples/members.rb
@@ -4,8 +4,6 @@ require 'rubygems'
 require 'yammer4r'
 
 yammer = Yammer::Client.new(:config => "oauth.yml")
-users1 = yammer.users(:page => 1)
-users2 = yammer.users(:page => 2)
-all = users1 + users2
+all = yammer.users(:page => 1) + yammer.users(:page => 2)
 contacts = all.map {|u| "#{u.name} #{u.contact.email_addresses.first.address}"};
 File.open("contacts.txt", "w") { |f| f.write(contacts.join("\n")) }
